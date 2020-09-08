6f4313f11f8b053a931fa856cbac3b8f8e2e2167
RobertGageStroud <stroudrg@appstate.edu>
2020-09-08 16:15:23 -0400

    Added unit tests to the project.

---
+++ CHIP-8 Unit Tests/CHIP-8 Unit Tests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace CHIP8UnitTests
+{
+	TEST_CLASS(CHIP8UnitTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
---
+++ MemoryUnitTest/MemoryUnitTest.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace MemoryUnitTest
+{
+	TEST_CLASS(MemoryUnitTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
